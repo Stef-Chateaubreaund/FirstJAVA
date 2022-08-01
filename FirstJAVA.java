82c0dc7a6fc968bdae7cc71f54c6e8776e3f48f7
StefannyDef <stefannydefreitas@gmail.com>
2022-08-01 08:41:30 -0400

    first commit

---
+++ FirstJAVA.java
@@ -0,0 +1,8 @@
+public class FirstJAVA {
+    public static void main(String [] args){
+        System.out.println("Hey, my name is Stefanny Chateaubreaund");
+        System.out.println("Im 27yo");
+        System.out.println("My hometown is in France");
+        
+    }
+}
